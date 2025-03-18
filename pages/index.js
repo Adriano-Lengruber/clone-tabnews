d46abda5c8ed0e69102720ec50fba3a8fc5d74fd
Adriano Lengruber <adrianolengruber@hotmail.com>
2025-03-18 19:40:47 +0000

    adicao da pasta pages

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div>
+      <h1>Bem vindo ao meu Next.js App!</h1>
+      <p>Esta é a página inicial.</p>
+      <h4>Por: Adriano Lengruber</h4>
+    </div>
+  );
+}
+
+export default Home;
